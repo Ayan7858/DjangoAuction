0db4ac5754b78c56983fdc0a936e90f71088d3ab
ShashankRaii <shashankrai087@gmail.com>
2022-10-27 21:25:44 +0530

    Deployment Test

--- commerce/urls.py
+++ commerce/urls.py
@@ -23,10 +23,15 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.views.generic import TemplateView
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls")),
     path('', TemplateView.as_view(template_name='template.html')),
+     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 # needed to display images from the models
