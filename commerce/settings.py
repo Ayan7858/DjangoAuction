b952b3eae9044344949cc374510c4a8208bf597d
ShashankRaii <shashankrai087@gmail.com>
2022-10-29 19:23:38 +0530

    this is a new test commit

--- commerce/settings.py
+++ commerce/settings.py
@@ -25,9 +25,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
+<<<<<<< HEAD
+ALLOWED_HOSTS = ['*']
+=======
 ALLOWED_HOSTS = ['minicarpoint.herokuapp.com']
+>>>>>>> bdc14f3c8b3681f986e5fcd3fbcf05387b4d9dbe
 
 
 # Application definition
@@ -44,7 +48,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
