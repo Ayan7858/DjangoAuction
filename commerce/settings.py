8866a40df321e542ed5466aca5cf2c003209d94f
ShashankRaii <shashankrai087@gmail.com>
2022-10-29 21:06:17 +0530

    j

--- commerce/settings.py
+++ commerce/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'auctions/uploads', 'auctions/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
 # needed to store images from the image field in the model
 MEDIA_ROOT = os.path.join(BASE_DIR, 'auctions/uploads', 'auctions/static')
