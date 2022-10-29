f8b694e9c80ea0c8cdc2ad10e3ca93dabd84087a
Ayan7858 <ayanmaghrabi7858@gmail.com>
2022-10-29 14:13:29 +0530

    sec

--- commerce/settings.py
+++ commerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://mini-carpoint.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'mini-carpoint.herokuapp.com']
 
 
 # Application definition
